1d749a9f8f6f5fbcfc5a6f4c5b6f9c72892b45a2
vintg <10236284+vintg@users.noreply.github.com>
2025-01-30 19:28:42 -0500

    feat: pulsing download button

---
+++ src/app/components/PulsingDownload.tsx
@@ -0,0 +1,48 @@
+import { Download } from "lucide-react"; 
+import { useEffect, useState } from "react";
+
+const PulsingDownload = () => {
+  const [pulsing, setPulsing] = useState(true);
+  const [hovered, setHovered] = useState(false); 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPulsing((prev) => !prev); 
+    }, 1000); 
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div
+      style={{
+        minWidth: "64px", 
+        minHeight: "64px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <a
+        href="https://drive.google.com/uc?id=1_6vKuOfQ0d6FogO61L0bbonMW4J0d96p&export=download"
+        className={`group flex items-center justify-center bg-blue-600 transition-all duration-${hovered? 300: 700} ease-in-out rounded-full`}
+        style={ hovered ? {
+          width: "32px",
+          height: "32px",
+          padding: "6px"
+        }:{
+          width: pulsing ? "12px" : "16px", 
+          height: pulsing ? "12px" : "16px",
+        }}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+      >
+        {/* Download Icon - Hidden until hovered */}
+        <Download
+          className="w-0 h-0 opacity-0 transition-all duration-300 ease-in-out group-hover:w-8 group-hover:h-8 group-hover:opacity-100 text-white"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default PulsingDownload;

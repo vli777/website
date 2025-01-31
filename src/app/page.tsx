d5e2c33399e8b880c469290168e003681cf93c52
vintg <10236284+vintg@users.noreply.github.com>
2025-01-30 19:29:07 -0500

    refactor: integrate download and clean up landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React from 'react';
+import React  from 'react';
 import DeepMatrixVisualization from './components/DeepMatrixVisualization';
+import PulsingDownload from './components/PulsingDownload';
 
 
 const Home: React.FC = () => {
@@ -9,20 +10,13 @@ const Home: React.FC = () => {
     <div className="flex flex-col md:flex-row h-screen bg-black">
       {/* Left Column: Content */}
       <div className="flex flex-col justify-center items-start px-16 md:w-1/2 text-white space-y-4">
-        <h1 className="text-4xl font-bold italic"></h1>
-        <p className="text-md">
-          Hi, I’m a full stack engineer specializing in UI development, visualization, and machine learning. 
-          Feel free to explore my work and reach out if you’d like to collaborate.
-        </p>
-        <blockquote className="italic text-gray-500">
-        天生我材必有用，千金散尽还复来
-        </blockquote>
-        <a
-          href="https://drive.google.com/uc?id=1_6vKuOfQ0d6FogO61L0bbonMW4J0d96p&export=download"
-          className="px-6 py-2 bg-blue-600 hover:bg-blue-700 rounded text-white text-lg"
-        >
-          Resume
-        </a>      
+        <div className="flex items-center">
+          <h1 className="text-4xl font-bold ">VINCE</h1>     
+          <PulsingDownload />
+        </div>        
+        <p className="text-md">  
+          I’m a full stack engineer specializing in UI development, visualization, and currently working on machine learning.
+        </p>             
       </div>
 
       {/* Right Column: 3D Animation */}
@@ -36,13 +30,13 @@ const Home: React.FC = () => {
             verticalSpacing={64}
             stackSpacing={64}
             layerSpacing={64}
-            maxConnectionsPerToken={8}
+            maxConnectionsPerToken={16}
             connectionColor="blue"
-            cameraZoom={3}
-            rotationSpeed={0.002}
-            lodDistanceThreshold={2000}
-            activationChance={0.02}
-            fadeSpeed={0.05}
+            cameraZoom={2.0}
+            rotationSpeed={0.007}
+            lodDistanceThreshold={1337}
+            activationChance={0.01}
+            fadeSpeed={0.5}
           />
         </div>
       </div>

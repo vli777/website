a9dd0021d9d1ee32a53b1700dc019b612cf79d5f
vintg <10236284+vintg@users.noreply.github.com>
2025-03-03 01:00:31 -0500

    style: mobile viewport nn vis placement and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,28 @@ const DeepMatrixVisualization = dynamic(
 const Home: React.FC = () => {
   return (
     <div className="px-8 md:px-16 lg:px-32">
-      <div className="flex flex-col md:flex-row h-screen">
+      <div className="relative flex flex-col my-3 md:flex-row h-auto md:h-screen">
+        {/* Mobile: DeepMatrix Animation as background */}
+        <div className="absolute inset-x-0 top-7 md:hidden">
+          <DeepMatrixVisualization
+            stackCount={1}
+            layerCount={8}
+            tokenCount={64}
+            horizontalSpacing={64}
+            verticalSpacing={64}
+            stackSpacing={64}
+            layerSpacing={64}
+            maxConnectionsPerToken={8}
+            connectionColor="blue"
+            cameraZoom={3.0}
+            rotationSpeed={0.007}
+            activationChance={0.01}
+            fadeSpeed={0.5}
+          />
+        </div>
+
         {/* Left Column: Content */}
-        <div className="flex flex-col justify-center items-start text-white space-y-4 md:w-1/2">
+        <div className="relative z-10 flex flex-col justify-center items-start text-white space-y-3 md:w-1/2 my-7 md:my-3">
           <div className="flex items-center">
             <h1 className="text-4xl font-bold">VINCE</h1>
             <PulsingDownload />
@@ -26,30 +45,28 @@ const Home: React.FC = () => {
           </p>
         </div>
 
-        {/* Right Column: 3D Animation */}
-        <div className="relative md:w-1/2 h-1/2 md:h-full">
-          <div className="absolute inset-0">
-            <DeepMatrixVisualization
-              stackCount={1}
-              layerCount={8}
-              tokenCount={64}
-              horizontalSpacing={64}
-              verticalSpacing={64}
-              stackSpacing={64}
-              layerSpacing={64}
-              maxConnectionsPerToken={8}
-              connectionColor="blue"
-              cameraZoom={3.0}
-              rotationSpeed={0.007}
-              activationChance={0.01}
-              fadeSpeed={0.5}
-            />
-          </div>
+        {/* Desktop: Right Column with DeepMatrix Animation */}
+        <div className="hidden md:block relative md:w-1/2 h-full">
+          <DeepMatrixVisualization
+            stackCount={1}
+            layerCount={8}
+            tokenCount={64}
+            horizontalSpacing={64}
+            verticalSpacing={64}
+            stackSpacing={64}
+            layerSpacing={64}
+            maxConnectionsPerToken={8}
+            connectionColor="blue"
+            cameraZoom={3.0}
+            rotationSpeed={0.007}
+            activationChance={0.01}
+            fadeSpeed={0.5}
+          />
         </div>
       </div>
 
-      {/* Parallax Image Cards */}
-      <div className="relative z-10 space-y-6 py-8">
+      {/* Parallax Image Cards */}      
+      <div className="relative z-10 space-y-6 py-8 mt-64 md:mt-0">
         {imageData.map((img, index) => (
           <ImageCard key={index} {...img} index={index} />
         ))}
@@ -57,5 +74,7 @@ const Home: React.FC = () => {
     </div>
   );
 };
+
+
 
 export default Home;

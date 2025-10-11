79b3e4b8a78224dbccf7a67c39e6a4dee94f33a3
vintg <10236284+vintg@users.noreply.github.com>
2025-10-11 16:03:48 -0400

    chore: update drive link

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -2,12 +2,12 @@ export function formatGoogleDriveLink(link: string): string {
   // Try matching the /d/ pattern
   let match = link.match(/\/d\/([^/]+)/);
   if (match && match[1]) {
-    return `https://drive.google.com/uc?id=${match[1]}&export=download`;
+    return `https://drive.google.com/uc?export=download&id=${match[1]}`;
   }
   // Try matching the open?id= pattern
   match = link.match(/open\?id=([^&]+)/);
   if (match && match[1]) {
-    return `https://drive.google.com/uc?id=${match[1]}&export=download`;
+    return `https://drive.google.com/uc?export=download&id=${match[1]}`;
   }
   // If no match, return the original link
   return link;
